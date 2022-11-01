d8bcc355d04788e07b52927aa9bdb22469422aaa
Kuta Yamane <hw20a145@oecu.jp>
2022-11-01 14:10:22 +0900

    Update:エフェクトを変更しました

--- REWorld/Assets/Personal/Yamane/Script/EffectChange.cs
+++ REWorld/Assets/Personal/Yamane/Script/EffectChange.cs
@@ -4,10 +4,31 @@ using UnityEngine;
 
 public class EffectChange : MonoBehaviour
 {
+    [Header("いつでも発生するエフェクト")]
+    [SerializeField]
+    ParticleSystem NormalEffect;
+
+    /*[Header("変化前のエフェクト")]
+    [SerializeField]
+    ParticleSystem BeforeEffect;*/
+
+    /*[Header("変化後のエフェクト")]
+    [SerializeField]
+    ParticleSystem AfterEffect;*/
+
+    //コライダーのサイズ
+    private float objectRadius;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        CircleCollider2D collider = GetComponent<CircleCollider2D>();
+        objectRadius = collider.radius;
+
+        var sh = NormalEffect.shape;
+        sh.radius = objectRadius;
+
+        Instantiate(NormalEffect, this.transform.position, Quaternion.identity); //パーティクル用ゲームオブジェクト生成
     }
 
     // Update is called once per frame
