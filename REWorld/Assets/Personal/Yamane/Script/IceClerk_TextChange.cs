00d75798dab7e487ce0d5c4bf5a08aae371a8ecb
Kuta Yamane <hw20a145@oecu.jp>
2022-12-07 11:00:40 +0900

    Update:アイス屋のセリフ切り替えを試しました

--- REWorld/Assets/Personal/Yamane/Script/IceClerk_TextChange.cs
+++ REWorld/Assets/Personal/Yamane/Script/IceClerk_TextChange.cs
@@ -1,18 +1,37 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
+using NPC;
 
 public class IceClerk_TextChange : MonoBehaviour
 {
+    [SerializeField]
+    IceClerk iceClerk;
+
+    [SerializeField]
+    private TextMeshProUGUI IceClerkText;
+
+    [Header("アイスが売れる前のセリフ")]
+    [SerializeField]
+    string BeforeText;
+
+    [Header("アイスが売れた後のセリフ")]
+    [SerializeField]
+    string AfterText;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        IceClerkText.text = BeforeText;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(iceClerk.INPCData.Name == "happy")
+        {
+            IceClerkText.text = AfterText;
+        }
     }
 }
