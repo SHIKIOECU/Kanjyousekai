cc8b82ca898420513b3f7b1f706344843415666b
Kuta Yamane <hw20a145@oecu.jp>
2022-12-11 18:19:22 +0900

    Add:所持アイテム表示、インタラクト表示の追加をしました

---
+++ REWorld/Assets/Personal/Yamane/Script/InteractUI.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InteractUI : MonoBehaviour
+{
+    [SerializeField]
+    FlagData _flagData;
+
+    [SerializeField]
+    SpriteRenderer sp;
+
+    private bool isGet;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        sp.enabled = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        isGet = _flagData.IsOn;
+
+        if(isGet == true)
+        {
+            sp.enabled = true;
+        }
+        else
+        {
+            sp.enabled = false;
+        }
+    }
+}
